740d140d3c52f30a4b9c0e604f8ee6fcdc36293f
Steph MANESCHI <stephanie.maneschi@hotmail.fr>
2022-07-11 10:53:44 +0200

    Add the class Joueur

---
+++ JeuDeDes/src/jeuDeDes/Joueur.java
@@ -0,0 +1,53 @@
+package jeuDeDes;
+
+public class Joueur {
+    private String nom;
+    private int score;
+
+
+    /**
+     * Constructeur avec 1 argument pour initialiser le nom d'un joueur
+     * et son score à 0
+     * @param nom Nom du joueur
+     */
+    public Joueur(String nom) {
+        this.nom = nom;
+        this.score = 0;
+    }
+
+    // ==== Getters ====
+
+    /**
+     * Getter permettant de retourner le nom du joueur
+     * @return Nom du joueur
+     */
+    public String get_nom() {
+        return(this.nom);
+    }
+
+    /**
+     * Getter permettant de retourner le score du joueur
+     * @return Score du joueur
+     */
+    public int get_score() {
+        return(this.score);
+    }
+
+    /**
+     * Méthode permettant de lancer le gobelet
+     * et mettre à jour le score du joueur
+     * @param gobelet Gobelet de la partie
+     */
+    public void jouer(Gobelet gobelet) {
+        gobelet.lancer();
+        this.score += gobelet.get_valeur();
+    }
+
+    /**
+     * Méthode permettant d'afficher le score d'un joueur
+     */
+    public void afficher_score() {
+        System.out.println("Le score du joueur " + this.nom + " est de : " + this.score);
+    }
+
+}
