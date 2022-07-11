96dd7ab6dcab2c973cbbbe505595fb612bb9bb1e
Steph MANESCHI <stephanie.maneschi@hotmail.fr>
2022-07-11 10:04:19 +0200

    Add the class De

---
+++ JeuDeDes/src/jeuDeDes/De.java
@@ -0,0 +1,33 @@
+package jeuDeDes;
+
+import java.util.Random;
+
+public class De {
+    private int valeur;
+
+    /**
+     * Constructeur sans arguments
+     * valeur est initialisé à 0
+     */
+    public De() {
+        this.valeur = 0;
+    }
+
+    /**
+     * Getter
+     * @return Valeur du dé
+     */
+    public int get_valeur() {
+        return(this.valeur);
+    }
+
+    /**
+     * Methode permettant de lancer un dé
+     * Donne une valeur aléatoire comprise entre 1 et 6 à l'attribut valeur
+     */
+    public void lancer() {
+        Random intRandom = new Random();
+        this.valeur = intRandom.nextInt(1, 6);
+    }
+
+}
